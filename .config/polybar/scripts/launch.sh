e55240334b1b5d13e4bd54961c88d9f988272194
TheBurgerMan <will76399@gmail.com>
2022-04-26 14:09:46 +0100

    changed bar name

--- .config/polybar/scripts/launch.sh
+++ .config/polybar/scripts/launch.sh
@@ -9,7 +9,7 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 #echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar -q example & #>>/tmp/polybar1.log 2>&1 & disown
+polybar -q mybar & #>>/tmp/polybar1.log 2>&1 & disown
 #polybar bar2 >>/tmp/polybar2.log 2>&1 & disown
 
 #echo "Bars launched..."
