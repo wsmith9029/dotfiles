3515498bf61494f04c280e3d2fd6b6992d49fe75
William Smith <will76399@gmail.com>
2026-06-06 23:37:33 +0100

    Removed random wallpaper timer

--- .config/hypr/modules/timers.lua
+++ .config/hypr/modules/timers.lua
@@ -1,4 +1,4 @@
 -- Random Wallpaper every X interval --
-hl.timer(function()
-    hl.exec_cmd("/home/will/.config/scripts/change-wallpaper.sh")
-end, { timeout = 3600000, type = "repeat" })
+-- hl.timer(function()
+--     hl.exec_cmd("/home/will/.config/scripts/change-wallpaper.sh")
+-- end, { timeout = 3600000, type = "repeat" })
